2c60fc15a8fea2cb94efe0fb1dd5dd07a0abd7f6
Vitalii <ghvitaliidrapaliuk@gmail.com>
2020-12-31 19:49:41 +0200

    add search form. Happy New Year!

--- src/components/todolist/components/todo_full_info/components/Subtasks.jsx
+++ src/components/todolist/components/todo_full_info/components/Subtasks.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { KEY_ENTER } from '../../../../../service/keyboard_codes'
 import { SubTask } from './subtask/SubTask';
-import { RiArrowDownSLine, RiArrowUpSLine } from 'react-icons/ri';
+import { RiArrowDownSLine, RiArrowUpSLine, RiArrowUpLine, RiCloseLine, RiFilterLine } from 'react-icons/ri';
+import { AiOutlineSortAscending,  AiOutlineSortDescending, AiOutlineSearch} from 'react-icons/ai'
+import { VscSearchStop, VscSearch } from 'react-icons/vsc'
 
 export function Subtasks({subtasks, onCreate, onUpdateText, onComplete, onDelete}) {
     const [text, writeText] = React.useState('')
-    const [openFullTextSubtaskID, setOpenFullText] = React.useState('');
-    const [isVisibleSubtasksList, setVisibleSubtasksList] = React.useState(false); 
+    const [openedSubtaskId, setOpenFullText] = React.useState('');
+    const [isVisibleList, setVisibleSubtasksList] = React.useState(false); 
+    const [isVisibleSorting, setVisibleSubtasksSorting] = React.useState(false); 
+    const [isVisibleSearchInput, setVisibleSearchInput] = React.useState(false); 
 
     const onOpenSubtask = id => setOpenFullText(id)
-    const visibleSubtasksListHandler = () => setVisibleSubtasksList(!isVisibleSubtasksList);
+    const visibleSubtasksListHandler = () => setVisibleSubtasksList(!isVisibleList);
+    const visibleSubtasksSortingHandler = () => setVisibleSubtasksSorting(!isVisibleSorting);
+    const visibleSearchInputHandler = () => setVisibleSearchInput(!isVisibleSearchInput)
+    const inVisibleSearchInputHandler = () => setVisibleSearchInput(false)
+
+    
     const createHandler = () => {
         onCreate(text)
         writeText('')
@@ -37,31 +46,62 @@ export function Subtasks({subtasks, onCreate, onUpdateText, onComplete, onDelete
             <button onClick = {visibleSubtasksListHandler} className = 'subtasks__open-btn'>
                 <span>3</span>
                 <span>subtask</span>
-                {isVisibleSubtasksList ? <RiArrowDownSLine className = 'subtasks__open-icon' /> : <RiArrowUpSLine className = 'subtasks__open-icon' />}
+                {isVisibleList ? <RiArrowDownSLine className = 'subtasks__open-icon' /> : <RiArrowUpSLine className = 'subtasks__open-icon' />}
             </button>
             {
-                isVisibleSubtasksList &&
+                isVisibleList &&
                 <ul class="todo-subtasks__list">
-                { subtasks.map(({_id, text, hasDone}) => <SubTask key = {_id}
-                                                           id = {_id} 
-                                                           onDelete = {onDelete}
-                                                           onComplete = {onComplete} 
-                                                           text = {text}
-                                                           onOpen = {onOpenSubtask}
-                                                           isOpen = {_id === openFullTextSubtaskID}
-                                                           hasDone = {hasDone}
-                                                           onUpdateText = {onUpdateText} />) }
-            </ul>
-            }
-            <div className = 'subtasks__open-btn'>
-            <button onClick = {visibleSubtasksListHandler}>
-                up
-            </button>
-            <button onClick = {visibleSubtasksListHandler}>
-                close
-            </button>
+                    { subtasks.map(({_id, text, hasDone}) => <SubTask key = {_id}
+                                                               id = {_id} 
+                                                               onDelete = {onDelete}
+                                                               onComplete = {onComplete} 
+                                                               text = {text}
+                                                               onOpen = {onOpenSubtask}
+                                                               isOpen = {_id === openedSubtaskId}
+                                                               hasDone = {hasDone}
+                                                               onUpdateText = {onUpdateText} />) }
+                    <li className = 'subtasks__settings-options'>
+                        {
+                            isVisibleSorting && !isVisibleSearchInput &&
+                            <div className = 'subtasks__sort-by'>
+                                <button className="subtasks__sort-by-item">
+                                    <AiOutlineSortAscending className = 'subtasks__settings-icon' />
+                                </button>
+                                <button className="subtasks__sort-by-item">
+                                    <AiOutlineSortDescending className = 'subtasks__settings-icon' />
+                                </button>
+                                <button onClick = {visibleSearchInputHandler} className="subtasks__sort-by-item">
+                                    <VscSearch className = 'subtasks__settings-icon' />
+                                </button>
+                            </div>
+                          
+                        }
+                        {
+                            isVisibleSearchInput &&
+                            <div className = 'subtasks__search-wrapper'>
+                                <button onClick = {inVisibleSearchInputHandler} className = 'subtasks__search-stop'>
+                                    <VscSearchStop className = 'subtasks__settings-icon' />
+                                </button>
+                                <input className = 'subtasks__search-input' placeholder = 'search' />
+                            </div>
+                        }
 
-            </div>
+
+                        <div className = 'subtasks__option-container'>
+                            <button className = 'subtasks__settings-option'>
+                                <RiArrowUpLine className = 'subtasks__settings-icon' />
+                            </button>
+                            <button onClick = {visibleSubtasksSortingHandler} className = 'subtasks__settings-option'>
+                                <RiFilterLine className = 'subtasks__settings-icon' />
+                            </button>
+                            <button className = 'subtasks__settings-option' onClick = {visibleSubtasksListHandler}>
+                                <RiCloseLine className = 'subtasks__settings-icon' />
+                            </button>
+                        </div>
+                    </li>
+                </ul>
+            }
+            
             
 
         </div>

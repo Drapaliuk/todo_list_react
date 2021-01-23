c91f567d8041980e854004db1f19fd979c25d2d6
Vitalii <ghvitaliidrapaliuk@gmail.com>
2021-01-24 00:23:28 +0200

    added styles to search input

--- src/components/organizer/tasks_list/todo_list_settings/SearchTaskByLetter.jsx
+++ src/components/organizer/tasks_list/todo_list_settings/SearchTaskByLetter.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { BsSearch } from 'react-icons/bs'
+import { GrClose } from 'react-icons/gr';
 
 export function SearchTaskByLetters() {
     const [text, writeText] = React.useState('')
     const writeTextHandler = e => writeText(e.value)
     return (
         <div className = 'search-task-by-letters'>
-            <BsSearch className = 'search-task-by-letters__icon' />
+            <BsSearch className = 'search-task-by-letters__icon-search' />
             <input value = {text} onChange = {writeTextHandler} className = 'search-task-by-letters__input' type="text"/>
+            <button className = 'search-task-by-letters__btn-cancel'>
+                <GrClose className = 'search-task-by-letters__icon-cancel' />
+            </button>
         </div>
     )
 }
